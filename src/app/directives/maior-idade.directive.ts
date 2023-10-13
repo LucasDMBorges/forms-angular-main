01cdc62e57c94c28f0c6461aec0c914e47f375d9
LucasDMBorges <dev.lucasdean@gmail.com>
2023-10-13 16:55:47 -0300

    feat: :sparkles: Validator com diretiva personalizada

---
+++ src/app/directives/maior-idade.directive.ts
@@ -0,0 +1,29 @@
+import { Directive } from "@angular/core";
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+} from "@angular/forms";
+
+@Directive({
+  selector: "[maiorIdadeValidator]",
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: MaiorIdadeDirective,
+      multi: true,
+    },
+  ],
+})
+export class MaiorIdadeDirective implements Validator {
+  constructor() {}
+  validate(control: AbstractControl): ValidationErrors | null {
+    const dataNascimento = control.value;
+    const anoNasciemnto = new Date(dataNascimento).getFullYear();
+    const anoNascMais18 = anoNasciemnto + 18;
+    const anoAtual = new Date().getFullYear();
+    const ehMaior = anoNascMais18 <= anoAtual;
+    return ehMaior ? null : { maiorIdadeValidator: true };
+  }
+}
